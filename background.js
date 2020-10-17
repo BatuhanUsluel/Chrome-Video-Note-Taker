cce090a022d34fe16f21aac1ab36539d294da5e5
BatuhanUsluel <batuhan.usluel@gmail.com>
2020-10-17 11:21:38 -0400

    Sidepanel
    https://stackoverflow.com/questions/39610205/how-to-make-side-panel-in-chrome-extension

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 // background.js
 
+
+chrome.browserAction.onClicked.addListener(function(tab){
+    chrome.tabs.sendMessage(tab.id,"toggle");
+});
+
 // Called when the user clicks on the browser action.
 /*
 chrome.browserAction.onClicked.addListener(function(tab) {
